c8f2fec695749d9703314b5048c9df784271a4c5
rembik <brian@rimek.info>
2017-05-26 15:45:36 +0200

    Update update-exchange-certificate.ps1

--- update-exchange-certificate.ps1
+++ update-exchange-certificate.ps1
@@ -10,12 +10,11 @@
                                  [-ExcludeLocalServerCert]
       
     All parameters in square brackets are optional.
-    The ExcludeLocalServerCert is $True if set. You 
-    almost never want this set to true, because Exchange
-    server hostname usally is equal to the certificate 
-    subject why local certificates could be the updated 
-    one. One Exception is using a wildcard certificate. 
-    It's there mainly for flexibility.
+    The ExcludeLocalServerCert is forced to $True if left off. You 
+    almost never want this set to false, because Exchange
+    server hostname usally isn't equal to the certificate 
+    subject why local certificates shouldn't be the updated 
+    one.
 
     If the password contains a $ sign, you must escape it with the `
     character.
@@ -59,7 +58,7 @@ if ([String]::IsNullOrEmpty($PFXPassword)) {
   $secPFXPassword = ConvertTo-SecureString -String $PFXPassword -Force -AsPlainText
 }
 
-if ($ExcludeLocalServerCert.IsPresent) { 
+if (!($ExcludeLocalServerCert.IsPresent)) { 
   $ExcludeLocalServerCert = $True
 }
 
